e363577f94b6b58fbc61a0c372f419663a6275be
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-03-17 16:26:25 +0100

    ranems endpoints

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -13,22 +13,29 @@ import { userSeasonFavoriteSchema } from '../schemas/userSeasonFavorite.schema';
 import { userMovieFavoriteSchema } from '../schemas/userMovieFavorite.schema';
 import { userEpisodeFavoriteSchema } from '../schemas/userEpisodeFavorite.schema';
 import { userGenreFavoriteSchema } from '../schemas/userGenreFavorite.schema';
+import { userSerieFavoriteSchema } from '../schemas/userSerie.schema';
 
 const router = express.Router();
 
 router.use(authMiddleware);
 
-router.get('/users', userQuerySchema, validateMiddleware, userController.getUsers);
+router.get('/getUsers', userQuerySchema, validateMiddleware, userController.getUsers);
 router.get('/getUserById/:id', userIdParamSchema, validateMiddleware, userController.getUserById);
 router.get('/getUserByTitle/:userName', userUserNameParamSchema, validateMiddleware, userController.getUserByTitle);
-router.delete('/users/:id', userIdParamSchema, validateMiddleware, userController.deleteUserById);
-router.patch('/users/:id', userIdParamSchema, userSchemaUpdate, validateMiddleware, userController.updateUserById);
-router.put('/users/:id', userIdParamSchema, userSchemaPost, validateMiddleware, userController.updateUserById);
-router.get('/searchUsers', userController.searchUsersByTitle);
-router.post('/addSeasonToUser', userSeasonFavoriteSchema, validateMiddleware, userController.addSeasonToUser);
-router.post('/addMovieToUser', userMovieFavoriteSchema, userController.addMovieToUser);
-router.post('/addSerieToUser', userEpisodeFavoriteSchema, validateMiddleware, userController.addSerieToUser);
-router.post('/addEpisodeToUser', userSeasonFavoriteSchema, validateMiddleware, userController.addEpisodeToUser);
-router.post('/addGenreToUser', userGenreFavoriteSchema, validateMiddleware, userController.addGenreToUser);
+router.delete('/deleteUserById/:id', userIdParamSchema, validateMiddleware, userController.deleteUserById);
+router.patch(
+    '/updateUserById/:id',
+    userIdParamSchema,
+    userSchemaUpdate,
+    validateMiddleware,
+    userController.updateUserById,
+);
+router.put('/updateUserById/:id', userIdParamSchema, userSchemaPost, validateMiddleware, userController.updateUserById);
+router.get('/searchUsersByTitle', userController.searchUsersByTitle);
+router.post('/bookmarkSeason', userSeasonFavoriteSchema, validateMiddleware, userController.bookmarkSeason);
+router.post('/bookmarkMovie', userMovieFavoriteSchema, userController.bookmarkSerie);
+router.post('/bookmarkSerie', userSerieFavoriteSchema, validateMiddleware, userController.bookmarkSerie);
+router.post('/bookmarkEpisode', userEpisodeFavoriteSchema, validateMiddleware, userController.bookmarkEpisode);
+router.post('/bookmarkGenre', userGenreFavoriteSchema, validateMiddleware, userController.bookmarkGenre);
 
 export default router;
