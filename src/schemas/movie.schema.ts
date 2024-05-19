b518464c8eed519b95fe1ad4351b38ed19224def
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-05-19 14:45:25 +0200

    update movie schema be

--- src/schemas/movie.schema.ts
+++ src/schemas/movie.schema.ts
@@ -3,14 +3,12 @@ import { body, param, query } from 'express-validator';
 const allowedSortByProperties = [
     'id',
     'title',
-    'videoSrc',
     'photoSrc',
     'trailerSrc',
     'duration',
     'ratingImdb',
     'releaseYear',
     'description',
-    'views',
 ];
 
 const movieQuerySchema = [
@@ -49,26 +47,22 @@ const movieTitleParamSchema = [
 
 const movieSchemaUpdate = [
     body('title').optional().isString(),
-    body('videoSrc').optional().isString(),
-    body('photoSrc').optional().isURL(),
+    body('photoSrc').optional().isString(),
     body('trailerSrc').optional().isURL(),
-    body('duration').optional().isString().isLength({ min: 1, max: 3 }),
+    body('duration').optional().isString().isLength({ min: 1, max: 10 }),
     body('ratingImdb').optional().isFloat({ min: 0, max: 10 }),
     body('releaseYear').optional().isInt({ min: 1900, max: new Date().getFullYear() }),
     body('description').optional().isString().isLength({ min: 10, max: 200 }),
-    body('views').optional().isInt({ min: 0 }),
 ];
 
 const movieSchemaPost = [
     body('title').isString(),
-    body('videoSrc').isString(),
-    body('photoSrc').isURL(),
+    body('photoSrc').isString(),
     body('trailerSrc').isURL(),
-    body('duration').isString().isLength({ min: 1, max: 3 }),
+    body('duration').isString().isLength({ min: 1, max: 10 }),
     body('ratingImdb').isFloat({ min: 0, max: 10 }),
     body('releaseYear').isInt({ min: 1900, max: new Date().getFullYear() }),
     body('description').isString().isLength({ min: 10, max: 200 }),
-    body('views').isInt({ min: 0 }),
 ];
 
 export { movieSchemaPost, movieSchemaUpdate, movieQuerySchema, movieIdParamSchema, movieTitleParamSchema };
