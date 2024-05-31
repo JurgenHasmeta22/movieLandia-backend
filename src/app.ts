6a10bc1b03dc2c212ded2fa859618156d70cd796
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-05-31 11:10:11 +0200

    image fix be

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,8 @@ const specs = swaggerJsDoc(options);
 export const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(express.static('public'));
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
