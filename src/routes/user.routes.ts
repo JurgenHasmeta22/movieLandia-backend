033a245a2b0c06e2a9c504d2984fe291a36b7789
JurgenHasmeta22 <jurgenhasmeta12@yahoo.com>
2024-06-01 13:10:25 +0200

    upvotes, downvotes be logic

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -18,11 +18,16 @@ import { userGenreFavoriteSchema } from '../schemas/userGenreFavorite.schema';
 import { userSerieFavoriteSchema } from '../schemas/userSerie.schema';
 import { movieReviewSchema } from '../schemas/movieReview.schema';
 import { serieReviewSchema } from '../schemas/serieReview.schema';
+import { upvoteMovieSchema } from '../schemas/upvoteMovie.schema';
+import { upvoteSerieSchema } from '../schemas/upvoteSerie.schema';
+import { downvoteMovieSchema } from '../schemas/downvoteMovie.schema';
+import { downvoteSerieSchema } from '../schemas/downvoteSerie.schema';
 
 const router = express.Router();
 
 // router.use(authMiddleware);
 
+// #region "CRUD Routes"
 router.get('/getUsers', userQuerySchema, validateMiddleware, userController.getUsers);
 router.get('/getUserById/:id', userIdParamSchema, validateMiddleware, userController.getUserById);
 router.get('/getUserByTitle/:userName', userUserNameParamSchema, validateMiddleware, userController.getUserByTitle);
@@ -36,8 +41,9 @@ router.patch(
 );
 router.put('/updateUserById/:id', userIdParamSchema, userSchemaPost, validateMiddleware, userController.updateUserById);
 router.get('/searchUsersByTitle', userController.searchUsersByTitle);
+// #endregion
 
-// #region "Bookmark routes"
+// #region "Bookmark Routes"
 router.post('/bookmarkSeason', userSeasonFavoriteSchema, validateMiddleware, userController.bookmarkSeason);
 router.post('/bookmarkMovie', userMovieFavoriteSchema, userController.bookmarkMovie);
 router.post('/bookmarkSerie', userSerieFavoriteSchema, validateMiddleware, userController.bookmarkSerie);
@@ -58,6 +64,17 @@ router.post('/removeReviewMovie', movieReviewSchema, validateMiddleware, userCon
 router.post('/removeReviewSerie', serieReviewSchema, validateMiddleware, userController.removeReviewSerie);
 router.post('/isSerieReviewed', validateMiddleware, userController.isSerieReviewed);
 router.post('/isMovieReviewed', validateMiddleware, userController.isMovieReviewed);
+// #endregion
+
+// #region "Upvotes, Downvotes Routes"
+router.post('/addUpvoteMovie', upvoteMovieSchema, validateMiddleware, userController.addUpvoteMovie);
+router.post('/addUpvoteSerie', upvoteSerieSchema, validateMiddleware, userController.addUpvoteSerie);
+router.post('/removeUpvoteMovie', upvoteMovieSchema, validateMiddleware, userController.removeUpvoteMovie);
+router.post('/removeUpvoteSerie', upvoteSerieSchema, validateMiddleware, userController.removeUpvoteSerie);
+router.post('/addDownvoteMovie', downvoteMovieSchema, validateMiddleware, userController.addDownvoteMovie);
+router.post('/addDownvoteSerie', downvoteSerieSchema, validateMiddleware, userController.addDownvoteSerie);
+router.post('/removeDownvoteMovie', downvoteMovieSchema, validateMiddleware, userController.removeDownvoteMovie);
+router.post('/removeDownvoteSerie', downvoteSerieSchema, validateMiddleware, userController.removeDownvoteSerie);
 // #endregion
 
 export default router;
---
+++ src/schemas/downvoteSerie.schema.ts
@@ -0,0 +1,5 @@
+import { body } from 'express-validator';
+
+const downvoteSerieSchema = [body('userId').isInt({ min: 1 }), body('serieReviewId').isInt({ min: 1 })];
+
+export { downvoteSerieSchema };
