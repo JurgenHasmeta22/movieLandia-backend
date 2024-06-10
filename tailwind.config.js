18e283b18ebb53b45d49c8d417eb1b6fa87c39ba
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-06-10 20:59:05 +0200

    fixed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/views/**/*.ejs', './public/**/*.html', './src/**/*.js'],
+    content: [
+        './src/views/**/*.ejs', 
+        './src/**/*.js',
+        './public/**/*.html',
+    ],
     theme: {
         extend: {},
     },
