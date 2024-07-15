b2b48422f8cc47312c90f69a12281b13341815bb
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-07-15 15:50:08 +0200

    problems in views not working at all edge

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
 import viewsRoutes from './routes/views.routes';
-import path from 'path';
-import 'dotenv/config';
 import session from 'express-session';
 import flash from 'connect-flash';
 import { Edge } from 'edge.js';
+import 'dotenv/config';
 
 export const prisma = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
@@ -15,7 +14,7 @@ export const prisma = new PrismaClient({
 export const app = express();
 
 const edge = new Edge({ cache: false });
-edge.mount(path.join(__dirname, 'views'));
+edge.mount(new URL('./views', import.meta.url));
 
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
@@ -37,14 +36,13 @@ app.engine('edge', (filePath, options, callback) => {
         .catch((err) => callback(err));
 });
 app.set('view engine', 'edge');
-app.set('views', path.join(__dirname, 'views'));
 
-// Middleware to make Edge globals available in all views
 app.use((req, res, next) => {
     res.locals = {
         ...res.locals,
         request: req,
     };
+
     next();
 });
 
